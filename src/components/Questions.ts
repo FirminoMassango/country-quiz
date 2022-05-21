5d145884c5ef80513208b797dec9ad20a8bcdbe0
FirminoMassango <firminorafael0@gmail.com>
2022-05-21 07:35:56 +0200

    first commit

---
+++ src/components/Questions.ts
@@ -0,0 +1,30 @@
+export const questions = [
+    {
+        questionText: "What is the capital of France?",
+        answerOptions: [
+            {answerText: "New York", isCorrect: false},
+            {answerText: "London", isCorrect: false},
+            {answerText: "Paris", isCorrect: true},
+            {answerText: "Bubai", isCorrect: false}
+        ]
+    },
+    {
+        questionText: "Who is the CEO of Tesla?",
+        answerOptions: [
+            {answerText: "Jeff Bezos", isCorrect: false},
+            {answerText: "Ellon Musk", isCorrect: true},
+            {answerText: "Bill Gates", isCorrect: true},
+            {answerText: "Tony Stark", isCorrect: false}
+        ]
+    },
+    {
+        questionText: "The iPhone was created by which company?",
+        answerOptions: [
+            {answerText: "Apple", isCorrect: true},
+            {answerText: "Intel", isCorrect: false},
+            {answerText: "Google", isCorrect: true},
+            {answerText: "Microsoft", isCorrect: false}
+        ]
+    }
+]
+
