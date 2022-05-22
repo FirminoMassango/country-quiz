c3f48182e2e618f23f7a10798ee42a576b8ad342
FirminoMassango <firminorafael0@gmail.com>
2022-05-22 22:55:22 +0200

    minor changes

--- src/components/Questions.ts
+++ src/components/Questions.ts
@@ -1,30 +1,60 @@
-export const questions = [
+type QuestionsType = 
+    {
+        questionText: string;
+        answerOptions: [
+            {
+                position: string;
+                answerText: string;
+                isCorrect: boolean;
+            },{
+                position: string;
+                answerText: string;
+                isCorrect: boolean;
+            },{
+                position: string;
+                answerText: string;
+                isCorrect: boolean;
+            },{
+                position: string;
+                answerText: string;
+                isCorrect: boolean;
+            }
+        ]
+        
+    }
+
+
+export const questions: QuestionsType[] = [
     {
         questionText: "What is the capital of France?",
         answerOptions: [
-            {answerText: "New York", isCorrect: false},
-            {answerText: "London", isCorrect: false},
-            {answerText: "Paris", isCorrect: true},
-            {answerText: "Bubai", isCorrect: false}
+            {position: "A", answerText: "New York", isCorrect: false},
+            {position: "B", answerText: "London", isCorrect: false},
+            {position: "c", answerText: "Paris", isCorrect: true},
+            {position: "D", answerText: "Bubai", isCorrect: false}
         ]
     },
     {
         questionText: "Who is the CEO of Tesla?",
         answerOptions: [
-            {answerText: "Jeff Bezos", isCorrect: false},
-            {answerText: "Ellon Musk", isCorrect: true},
-            {answerText: "Bill Gates", isCorrect: true},
-            {answerText: "Tony Stark", isCorrect: false}
+            {position: "A", answerText: "Jeff Bezos", isCorrect: false},
+            {position: "B", answerText: "Ellon Musk", isCorrect: true},
+            {position: "C", answerText: "Bill Gates", isCorrect: true},
+            {position: "D", answerText: "Tony Stark", isCorrect: false}
         ]
     },
     {
         questionText: "The iPhone was created by which company?",
         answerOptions: [
-            {answerText: "Apple", isCorrect: true},
-            {answerText: "Intel", isCorrect: false},
-            {answerText: "Google", isCorrect: true},
-            {answerText: "Microsoft", isCorrect: false}
+            {position: "A", answerText: "Apple", isCorrect: true},
+            {position: "B", answerText: "Intel", isCorrect: false},
+            {position: "C", answerText: "Google", isCorrect: true},
+            {position: "D", answerText: "Microsoft", isCorrect: false}
         ]
     }
+  
 ]
+
+
+
 
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,4 +1,6 @@
 import "../App.css";
+import { questions } from "./Questions";
+
 function Quiz() {
   return (
     <div className="App flex justify-center items-center">
@@ -6,38 +8,18 @@ function Quiz() {
         <h1 className="text-xl text-slate-500 font-bold">
           Kuala Lumpur is the capital of 🇵🇪
         </h1>
-        <div className="m-10 py-3 px-5 border-2 border-indigo-400 hover:text-white hover:bg-yellow-500 hover:border-yellow-500 flex rounded-lg">
-          <span className="mr-5 text-base text-indigo-400 hover:white font-semibold">
-            A
-          </span>
-          <span className="mr-5 text-base text-indigo-400 hover:white font-semibold">
-            Vietnam
-          </span>
-        </div>
-        <div className="m-10 py-3 px-5 border-2 border-indigo-400 flex rounded-lg">
-          <span className="mr-5 text-base text-indigo-400 font-semibold">
-            B
-          </span>
-          <span className="mr-5 text-base text-indigo-400 font-semibold">
-            Malaysia
-          </span>
-        </div>
-        <div className="m-10 py-3 px-5 border-2 border-indigo-400 flex rounded-lg">
-          <span className="mr-5 text-base text-indigo-400 font-semibold">
-            C
-          </span>
-          <span className="mr-5 text-base text-indigo-400 font-semibold">
-            Sweden
-          </span>
-        </div>
-        <div className="m-10 py-3 px-5 border-2 border-indigo-400 flex rounded-lg">
-          <span className="mr-5 text-base text-indigo-400 font-semibold">
-            D
-          </span>
-          <span className="mr-5 text-base text-indigo-400 font-semibold">
-            Austria
-          </span>
-        </div>
+        {questions.map((question) => {
+          return (
+            <div className="m-10 py-3 px-5 border-2 border-indigo-400 hover:text-white hover:bg-yellow-500 hover:border-yellow-500 flex rounded-lg">
+              <span className="mr-5 text-base text-indigo-400 hover:white font-semibold">
+                {question.answerOptions.position}
+              </span>
+              <span className="mr-5 text-base text-indigo-400 hover:white font-semibold">
+                Vietnam
+              </span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
